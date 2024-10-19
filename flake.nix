4542f2e2bf47efe91eb70ad47358eae893412dae
ruben <ruben.cano96@gmail.com>
2024-10-19 17:32:28 +0000

    Add flake.nix to build the project

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "fsembed";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.llvmPackages_19.stdenv.mkDerivation {
+          name = "fsembed";
+          pname = "fsembed";
+
+          src = self;
+          buildPhase = ''
+            clang++ -std=c++23 -O3 -o fsembed fsembed.cpp
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp fsembed $out/bin
+          '';
+        };
+      });
+    };
+}
